953cbb8c0f2ce016a54645c0d629f9eab6a9b846
zoltaneusz <fzoltan88@gmail.com>
2016-10-31 15:04:22 +0100

    Google maps working

--- app/src/main/java/zollie/travelblogger/guidee/ExploreFragment.java
+++ app/src/main/java/zollie/travelblogger/guidee/ExploreFragment.java
@@ -9,33 +9,79 @@ import android.view.ViewGroup;
 
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
- * Created by zsomborfuszenecker on 10/29/16.
+ * Created by zoltanfuszenecker on 10/29/16.
  */
 
 
 
-public class ExploreFragment extends Fragment implements OnMapReadyCallback {
+public class ExploreFragment extends Fragment{
 
 
+    MapView mMapView;
+    private GoogleMap googleMap;
 
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+
+        mMapView = (MapView) rootView.findViewById(R.id.exploreMap);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume(); // needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                googleMap = mMap;
+
+                // For showing a move to my location button
+                googleMap.setMyLocationEnabled(true);
+
+                // For dropping a marker at a point on the Map
+                LatLng sydney = new LatLng(-34, 151);
+                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+
+                // For zooming automatically to the location of the marker
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        });
+
+        return rootView;
     }
 
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        MapFragment fragment = (MapFragment) getChildFragmentManager().findFragmentById(R.id.exploremap);
-        fragment.getMapAsync(this);
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
     }
 
     @Override
-       public void onMapReady(GoogleMap googleMap) {
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
 
-       }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
 }
--- app/src/main/java/zollie/travelblogger/guidee/MainActivity.java
+++ app/src/main/java/zollie/travelblogger/guidee/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
 
 import com.roughike.bottombar.BottomBar;
@@ -13,25 +14,31 @@ import com.roughike.bottombar.OnTabSelectListener;
 
 
 /**
- * Created by zsomborfuszenecker on 10/29/16.
+ * Created by zoltanfuszenecker on 10/29/16.
  */
 
 public class MainActivity extends Activity {
 
-
+    private ExploreFragment _exploreMap = new ExploreFragment();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Pushing MapView Fragment
+        Fragment fragment = Fragment.instantiate(this, ExploreFragment.class.getName());
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.contentContainer, fragment);
+        ft.commit();
+
 
         BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
         bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
             public void onTabSelected(@IdRes int tabId) {
-                FragmentManager ft = getFragmentManager();
-                ft.beginTransaction().replace(R.id.contentContainer, new ExploreFragment()).commit();
+                FragmentManager fm = getFragmentManager();
+                fm.beginTransaction().replace(R.id.contentContainer, _exploreMap).commit();
 
             }
         });
