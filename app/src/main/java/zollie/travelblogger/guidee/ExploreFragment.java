b14aeb4ce27d3ab150afe0852b0a2386053947b4
zoltaneusz <fzoltan88@gmail.com>
2016-10-31 16:48:15 +0100

    Map marker added

--- app/src/main/java/zollie/travelblogger/guidee/ExploreFragment.java
+++ app/src/main/java/zollie/travelblogger/guidee/ExploreFragment.java
@@ -1,6 +1,13 @@
 package zollie.travelblogger.guidee;
 
 import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Shader;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -9,6 +16,8 @@ import android.view.ViewGroup;
 
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -48,9 +57,48 @@ public class ExploreFragment extends Fragment{
                 // For showing a move to my location button
                 googleMap.setMyLocationEnabled(true);
 
+
+
                 // For dropping a marker at a point on the Map
                 LatLng sydney = new LatLng(-34, 151);
-                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+         //       googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_friends)));
+
+               /*Creating custom map markers here */
+
+                Bitmap.Config conf = Bitmap.Config.ARGB_8888;
+                Bitmap bmp = Bitmap.createBitmap(80, 80, conf);
+                Canvas canvas1 = new Canvas(bmp);
+
+                // paint defines the text color, stroke width and size
+                Paint color = new Paint();
+                color.setTextSize(35);
+                color.setColor(Color.BLACK);
+
+                // modify canvas
+                canvas1.drawBitmap(BitmapFactory.decodeResource(getResources(),
+                        R.drawable.profile_pic), 0,0, color);
+                //canvas1.drawText("Zollie", 30, 40, color);
+
+
+                Bitmap bitmap = bmp;
+                Bitmap circleBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+
+                BitmapShader shader = new BitmapShader (bitmap,  Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+                Paint paint = new Paint();
+                paint.setShader(shader);
+                paint.setAntiAlias(true);
+                Canvas c = new Canvas(circleBitmap);
+                c.drawCircle(bitmap.getWidth()/2+4, bitmap.getHeight()/2+4, bitmap.getWidth()/2+4, new Paint());
+                c.drawCircle(bitmap.getWidth()/2, bitmap.getHeight()/2, bitmap.getWidth()/2, paint);
+
+
+                // add marker to Map
+                mMap.addMarker(new MarkerOptions().position(sydney)
+                        .icon(BitmapDescriptorFactory.fromBitmap(circleBitmap))
+                        .title("Zollie")
+                        .snippet("Journey to Sydney")
+                        // Specifies the anchor to be at a particular point in the marker image.
+                        .anchor(0.5f, 1));
 
                 // For zooming automatically to the location of the marker
                 CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
