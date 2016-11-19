934f70685668558b11122ba7dbc988e050029a66
Zoltaneusz <fzoltan88@gmail.com>
2016-11-19 23:20:22 +0100

    Animating Marker
    First try on animating marker:  on touch destroy old marker and draw new
    with bigger frame.
    
    Attempts to do this in layout or another class not working.

--- app/src/main/java/zollie/travelblogger/guidee/ExploreFragment.java
+++ app/src/main/java/zollie/travelblogger/guidee/ExploreFragment.java
@@ -20,6 +20,7 @@ import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
@@ -28,15 +29,25 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 
 
-public class ExploreFragment extends Fragment{
+public class ExploreFragment extends Fragment {
 
 
     MapView mMapView;
     private GoogleMap googleMap;
 
+    Bitmap.Config conf = Bitmap.Config.ARGB_8888;
+    Bitmap bmp = Bitmap.createBitmap(90, 90, conf);
+    Canvas canvas1 = new Canvas(bmp);
+
+    // paint defines the text color, stroke width and size
+    Paint color = new Paint();
+    Bitmap circleBitmap;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+
+
+
 
         mMapView = (MapView) rootView.findViewById(R.id.exploreMap);
         mMapView.onCreate(savedInstanceState);
@@ -49,56 +60,59 @@ public class ExploreFragment extends Fragment{
             e.printStackTrace();
         }
 
+
         mMapView.getMapAsync(new OnMapReadyCallback() {
             @Override
-            public void onMapReady(GoogleMap mMap) {
+            public void onMapReady(final GoogleMap mMap) {
                 googleMap = mMap;
 
                 // For showing a move to my location button
                 googleMap.setMyLocationEnabled(true);
 
 
-
                 // For dropping a marker at a point on the Map
-                LatLng sydney = new LatLng(-34, 151);
+                final LatLng sydney = new LatLng(-34, 151);
+
+                googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                    @Override
+                    public boolean onMarkerClick(Marker marker) {
+
+                        marker.remove();
+
+                        // Animating marker implementation =================================================
+                        pulseMarker(1);
+                        Marker myMarker2 = mMap.addMarker(new MarkerOptions().position(sydney)
+                                .icon(BitmapDescriptorFactory.fromBitmap(circleBitmap))
+                                .title("Zollie")
+                                .snippet("Journey to Sydney")
+                                // Specifies the anchor to be at a particular point in the marker image.
+                                .anchor(0.4f, 1));
+
+                        //==================================================================================
+
+                        return false;
+                    }
+                });
+
          //       googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_friends)));
 
                /*Creating custom map markers here */
+                // =================================================================================
 
-                Bitmap.Config conf = Bitmap.Config.ARGB_8888;
-                Bitmap bmp = Bitmap.createBitmap(80, 80, conf);
-                Canvas canvas1 = new Canvas(bmp);
-
-                // paint defines the text color, stroke width and size
-                Paint color = new Paint();
-                color.setTextSize(35);
-                color.setColor(Color.BLACK);
-
-                // modify canvas
-                canvas1.drawBitmap(BitmapFactory.decodeResource(getResources(),
-                        R.drawable.profile_pic), 0,0, color);
-                //canvas1.drawText("Zollie", 30, 40, color);
-
-
-                Bitmap bitmap = bmp;
-                Bitmap circleBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
-
-                BitmapShader shader = new BitmapShader (bitmap,  Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
-                Paint paint = new Paint();
-                paint.setShader(shader);
-                paint.setAntiAlias(true);
-                Canvas c = new Canvas(circleBitmap);
-                c.drawCircle(bitmap.getWidth()/2+4, bitmap.getHeight()/2+4, bitmap.getWidth()/2+4, new Paint());
-                c.drawCircle(bitmap.getWidth()/2, bitmap.getHeight()/2, bitmap.getWidth()/2, paint);
-
+                pulseMarker(0);
 
                 // add marker to Map
-                mMap.addMarker(new MarkerOptions().position(sydney)
+                Marker myMarker = mMap.addMarker(new MarkerOptions().position(sydney)
                         .icon(BitmapDescriptorFactory.fromBitmap(circleBitmap))
                         .title("Zollie")
                         .snippet("Journey to Sydney")
                         // Specifies the anchor to be at a particular point in the marker image.
-                        .anchor(0.5f, 1));
+                        .anchor(0.4f, 1));
+
+                //==================================================================================
+
+
+
 
                 // For zooming automatically to the location of the marker
                 CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
@@ -109,16 +123,21 @@ public class ExploreFragment extends Fragment{
         return rootView;
     }
 
+    //================================================
+
+
     @Override
     public void onResume() {
         super.onResume();
         mMapView.onResume();
+        //MarkerAnimation.resume();
     }
 
     @Override
     public void onPause() {
         super.onPause();
         mMapView.onPause();
+        //MarkerAnimation.pause();
     }
 
     @Override
@@ -132,4 +151,37 @@ public class ExploreFragment extends Fragment{
         super.onLowMemory();
         mMapView.onLowMemory();
     }
+    public void pulseMarker(int step){
+
+
+        color.setTextSize(35);
+        color.setColor(Color.GRAY);
+
+        // modify canvas
+        canvas1.drawBitmap(BitmapFactory.decodeResource(getResources(),
+                R.drawable.profile_pic), 0,0, color);
+        //canvas1.drawText("Zollie", 30, 40, color);
+
+
+        Bitmap bitmap = bmp;
+        circleBitmap = Bitmap.createBitmap(bitmap.getWidth()+5, bitmap.getHeight()+5, Bitmap.Config.ARGB_8888);
+
+        BitmapShader shader = new BitmapShader (bitmap,  Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        Paint profilePic = new Paint();
+        profilePic.setShader(shader);
+        profilePic.setAntiAlias(true);
+        Canvas c = new Canvas(circleBitmap);
+        Paint circleFrame = new Paint();
+        circleFrame.setColor(Color.GRAY);
+
+        c.drawCircle((int)(bitmap.getWidth()/2.5), (int)(bitmap.getHeight()/2.5),(int) (bitmap.getWidth()/2.5)-1+step*10, circleFrame);
+        circleFrame.setColor(Color.LTGRAY);
+        circleFrame.setStyle(Paint.Style.STROKE);
+        c.drawCircle((int)(bitmap.getWidth()/2.5), (int)(bitmap.getHeight()/2.5),(int) (bitmap.getWidth()/2.5)-4+step*10, circleFrame);
+        c.drawCircle((int)(bitmap.getWidth()/2.5), (int)(bitmap.getHeight()/2.5),(int) (bitmap.getWidth()/2.5)+step*10, circleFrame);
+        c.drawCircle((int)(bitmap.getWidth()/2.5), (int)(bitmap.getHeight()/2.5), (int)(bitmap.getWidth()/2.5)-5+step*10, profilePic);
+
+    }
+
 }
+
